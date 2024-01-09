ccb8eaaa20ecae796f6bdf5704b99ab47d816517
Mohit Maniyar <142476526+MohitManiyar-uni@users.noreply.github.com>
2024-01-09 14:51:47 +0000

    nums done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,6 +1,18 @@
 const score = 400
 
-const balance = new Number(100)
-console.log(balance);
+// const balance = new Number(100)
+// console.log(balance);
 
-console.log(balance.toString());
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber= 23.8966
+
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++
+
+console.log(Math);
